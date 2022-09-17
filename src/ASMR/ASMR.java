41922c31c62f65930b32d95a10bc6ca0682be202
极乐鸟 <82135089+shenniao2021@users.noreply.github.com>
2022-09-18 06:42:38 +0800

    Add files via upload

---
+++ src/ASMR/ASMR.java
@@ -0,0 +1,23 @@
+package ASMR;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URLConnection;
+import java.util.List;
+
+/**
+ * @Author wanglin
+ * 2022/8/23 23:16
+ */
+public interface  ASMR {
+     String getCookie();
+     void setCookie(String cookie);
+    HttpURLConnection config(URLConnection urlConnection);
+
+    int fileCounts(String sb);
+     void download(OutputStream out, InputStream inStrm, int fileLength) throws IOException;
+    List<String> findTsName(String m3u8Address, String m3u8);
+    String findM3u8(String sb2);
+}
---
+++ src/Start.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author wanglin
+ * 2022/8/24 4:29
+ */
+public class Start {
+    public static void main(String[] args) {
+       new m3u8Download().strat();
+
+    }
+
+
+}
